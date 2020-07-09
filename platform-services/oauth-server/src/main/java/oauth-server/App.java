cb3ce00cfc98cd100771477afe8f7508e444a17c
Ubuntu <ubuntu@ip-172-31-39-58.ap-southeast-1.compute.internal>
2020-07-09 07:33:35 +0000

    lab

--- platform-services/oauth-server/src/main/java/oauth-server/App.java
+++ platform-services/oauth-server/src/main/java/oauth-server/App.java
@@ -7,6 +7,7 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 @SpringBootApplication
 public class App extends AuthorizationServerConfigurerAdapter {
 
+
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
